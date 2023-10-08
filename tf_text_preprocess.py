55269ae68695ff3e99636bffeb5aacaeec2cbc25
oblvisheee <molniya213y@gmail.com>
2023-10-09 00:12:37 +0300

    Added preprocess.

--- tf_text_preprocess.py
+++ tf_text_preprocess.py
@@ -7,10 +7,13 @@ import re
 
 class TextPreprocessor:
     def __init__(self, dataset_names=None, text_files_paths=None):
-        self.dataset_names = dataset_names if isinstance(dataset_names, list) else [dataset_names]
-        self.text_files_paths = text_files_paths if isinstance(text_files_paths, list) else [text_files_paths]
+        self.dataset_names = self._ensure_list(dataset_names)
+        self.text_files_paths = self._ensure_list(text_files_paths)
         self.datasets = []
         self.tokenizer = Tokenizer()
+
+    def _ensure_list(self, item):
+        return item if isinstance(item, list) else [item]
 
     def load_datasets(self):
         for dataset_name in self.dataset_names:
@@ -20,29 +23,66 @@ class TextPreprocessor:
     def load_text_files(self):
         for text_files_path in self.text_files_paths:
             if text_files_path:
-                self.datasets.append(tf.data.TextLineDataset(
-                    tf.data.Dataset.list_files(os.path.join(text_files_path, '*.txt'))
-                ))
+                self.datasets.append(self._load_text_file(text_files_path))
+
+    def _load_text_file(self, text_files_path):
+        return tf.data.TextLineDataset(
+            tf.data.Dataset.list_files(os.path.join(text_files_path, '*.txt'))
+        )
 
     def clean_text(self, text):
-        text = re.sub(r'\n', ' ', text)
-        text = re.sub(r'\r', ' ', text)
-        text = re.sub(r'\t', ' ', text)
-        text = re.sub(r' +', ' ', text)
+        text = self._remove_newlines(text)
+        text = self._remove_tabs(text)
+        text = self._remove_extra_spaces(text)
         return text
 
-    def tokenize_and_pad(self, text):
+    def _remove_newlines(self, text):
+        return re.sub(r'\n', ' ', text)
+
+    def _remove_tabs(self, text):
+        return re.sub(r'\t', ' ', text)
+
+    def _remove_extra_spaces(self, text):
+        return re.sub(r' +', ' ', text)
+
+    def tokenize_text(self, text):
         self.tokenizer.fit_on_texts(text)
         sequences = self.tokenizer.texts_to_sequences(text)
+        return sequences
+
+    def pad_sequences(self, sequences):
         padded_sequences = pad_sequences(sequences, padding='post')
         return padded_sequences
 
     def preprocess_datasets(self):
         for i, dataset in enumerate(self.datasets):
             cleaned_text = self.clean_text(dataset)
-            tokenized_and_padded_text = self.tokenize_and_pad(cleaned_text)
-            self.datasets[i] = tokenized_and_padded_text
+            tokenized_text = self.tokenize_text(cleaned_text)
+            padded_text = self.pad_sequences(tokenized_text)
+            self.datasets[i] = padded_text
 
     def get_datasets(self):
         return self.datasets
 
+class TextClass(TextPreprocessor):
+    def __init__(self, class_name, dataset_names=None, text_files_paths=None):
+        super().__init__(dataset_names, text_files_paths)
+        self.class_name = class_name
+
+    def load_text_files(self):
+        for text_files_path in self.text_files_paths:
+            if text_files_path:
+                self.datasets.append(self._load_text_file(text_files_path, self.class_name))
+
+    def _load_text_file(self, text_files_path, class_name):
+        return tf.data.TextLineDataset(
+            tf.data.Dataset.list_files(os.path.join(text_files_path, class_name, '*.txt'))
+        )
+
+class EnglishTexts(TextClass):
+    def __init__(self, class_name, dataset_names=None, text_files_paths=None):
+        super().__init__(class_name, dataset_names, text_files_paths)
+
+class RussianTexts(TextClass):
+    def __init__(self, class_name, dataset_names=None, text_files_paths=None):
+        super().__init__(class_name, dataset_names, text_files_paths)
