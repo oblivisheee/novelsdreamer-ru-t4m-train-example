5fdc009a169e3d3ba171ddac7bccbb1d5eeb7bb4
oblvisheee <molniya213y@gmail.com>
2023-10-11 11:43:01 +0300

    Added train config.

--- create_config.py
+++ create_config.py
@@ -1,8 +1,8 @@
 import os
 import json
-def transformer_config():
-    path_to_config_transformer = 'model_configs/transformer_config.json'
 
+def transformer_config():
+    path_to_config_transformer = './transformer_config.json'
     config = {
         "num_layers": 4,
         "d_model": 128,
@@ -14,7 +14,6 @@ def transformer_config():
         "maximum_position_encoding": 10000,
         "batch_size": 32,
         "tar_seq_len": 16
-
     }
 
     os.makedirs(os.path.dirname(path_to_config_transformer), exist_ok=True)
@@ -30,3 +29,27 @@ def transformer_config():
         with open(path_to_config_transformer, 'w') as f:
             json.dump(config, f)
     return config
+
+def trainer_config():
+    path_to_config_of_train = './train_config.json'
+    config = {
+        "learning_rate":  0.001,
+        "loss_reduction": 'auto',
+        "epoch_train": 30, 
+    }
+
+    os.makedirs(os.path.dirname(path_to_config_of_train), exist_ok=True)
+
+    if not os.path.isfile(path_to_config_of_train):
+        with open(path_to_config_of_train, 'w') as f:
+            json.dump(config, f)
+
+    try:
+        with open(path_to_config_of_train, 'r') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        with open(path_to_config_of_train, 'w') as f:
+            json.dump(config, f)
+    return config
+
+trainer_config()
