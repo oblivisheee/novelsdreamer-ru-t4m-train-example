8fda3c15c8b449ffbf9963ea1503202e1c59ea37
oblvisheee <molniya213y@gmail.com>
2023-10-11 11:44:09 +0300

    Added needed lib

--- transformer_custom.py
+++ transformer_custom.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
+from math import log
 from safetensors.tensorflow import save_file
 # Positional Encoding
 def get_angles(pos, i, d_model):
