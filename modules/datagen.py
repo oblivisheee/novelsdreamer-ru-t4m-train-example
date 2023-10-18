dabb7dd2cb4dd21e03c3af3f2307fbd7563f7a78
oblvisheee <molniya213y@gmail.com>
2023-10-18 23:32:44 +0300

    Fixed datagen.
    Now, its working.

--- modules/datagen.py
+++ modules/datagen.py
@@ -2,16 +2,24 @@ import os
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-"""
 
-Полностью переписать!(также как и инициализатор в блокноте)
-"""
 class DataGenerator:
+    """
+    Generates tensors by converting input data.
+
+    Usage:
+    >>> datagen = DataGenerator(TRAIN_DATASET_DIR, VALID_DATASET_DIR)
+
+    >>> (train_english, train_russian), (valid_english, valid_russian) = datagen.generate()
+
+    
+    """
     def __init__(self, train_dir, valid_dir, padding_type='post', trunc_type='post'):
         self.train_dir = train_dir
         self.valid_dir = valid_dir
         self.padding_type = padding_type
         self.trunc_type = trunc_type
+
 
     def load_data(self, dir_name):
         data = {}
@@ -34,9 +42,16 @@ class DataGenerator:
             if sequences:
                 padded = pad_sequences(sequences, padding=self.padding_type, truncating=self.trunc_type)
                 data[class_name] = [tf.expand_dims(p, -1) for p in padded]  # Add an extra dimension at the end to avoid ValueError
+        print(data)
         return data
 
+
     def generate(self):
+        """
+        Do main process of converting tensors.
+        Usage:
+        >>> (train_english, train_russian), (valid_english, valid_russian) = datagen.generate()
+        """
         train_data = self.load_data(self.train_dir)
         valid_data = self.load_data(self.valid_dir)
         train_data = self.prepare_data(train_data)
@@ -49,4 +64,3 @@ class DataGenerator:
         print(f"Valid data info: {len(valid_data.keys())} classes, {sum([len(v) for v in valid_data.values()])} samples")
 
         return (train_data, valid_data)
-
